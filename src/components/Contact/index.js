7fce4f7c6b3bed4b4b80ec867065221179636dc9
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-06-20 05:14:41 +0800

    Updated contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -14,18 +14,14 @@ const Contact = () => (
         <ContactWrapper>
           <InfosWrapper>
             <h1>Contact</h1>
-            <p>
-              Let's get in touch, for any tech matter or even simply say hi, If
-              you are feeling really adventurous, just drop me a line.
-            </p>
+            <p>Let's get in touch or even simply say hi...</p>
             <p>
               <a
                 href="https://www.linkedin.com/in/gsaileshkumar/"
                 target="__blank"
               >
-                Connect with me
-              </a>{" "}
-              .
+                Connect with me.
+              </a>
             </p>
           </InfosWrapper>
           <ImageWrapper>
