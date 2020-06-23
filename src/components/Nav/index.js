0036f52be82ba0131e42ef6f624b30337c8c6860
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-06-24 02:26:38 +0800

    updated duration and bg color

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,24 +1,27 @@
 import React from "react"
 
-import AniLink from "gatsby-plugin-transition-link/AniLink";
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Icons from "./icons"
 import links from "./content"
 import { Wrapper } from "./styled"
 
 const Nav = () => (
-
-    <Wrapper>
+  <Wrapper>
     <ul>
-        { 
-        links.map((link, i) => {
-            const Icon = Icons[link.label]
-            return (
-                <li key={i}> <AniLink paintDrip to={link.url} hex="#5A67D8"> <Icon />  {link.label} </AniLink> </li>
-            )
-        })
-        }
+      {links.map((link, i) => {
+        const Icon = Icons[link.label]
+        return (
+          <li key={i}>
+            {" "}
+            <AniLink paintDrip to={link.url} hex="#4c5fb0" duration={0.5}>
+              {" "}
+              <Icon /> {link.label}{" "}
+            </AniLink>{" "}
+          </li>
+        )
+      })}
     </ul>
-    </Wrapper> 
+  </Wrapper>
 )
 
 export default Nav
